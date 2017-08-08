042d0f980db834b20d80f9d89a14af3d6baca7ac
Morgan <vdl@vMacBook.local>
2017-08-08 15:42:41 -0400

    Fixed device scaling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,10 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Case Law, PLLC"
+    base_title = "Alpharetta, GA | Case Law, PLLC"
+    expanded_title = "Georgia Personal Injury, Family, and DUI Lawyer | Alpharetta, GA | Case Law, PLLC"
     if page_title.empty?
-      base_title
+      expanded_title
     else
       page_title + " | " + base_title
     end
