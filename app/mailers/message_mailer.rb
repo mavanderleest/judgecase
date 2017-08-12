84405378df95fb96123b4902a3e04d04c081c147
Morgan <vdl@vMacBook.local>
2017-08-11 23:56:05 -0400

    More ActionMailer production testing

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,10 @@ class MessageMailer < ApplicationMailer
     @email = message.email
     @body = message.body
 
-    mail to: "mavanderleest@gmail.com", from: 'morgan@judgecase.com'
+    mail to: 'mavanderleest@gmail.com,morgan@atomicdigital.io',
+                 cc: 'mvanderleest@semanticsgroup.com',
+                 bcc: 'morgan@judgecase.com',
+                 from: 'morgan@judgecase.com'
   end
 
 end
