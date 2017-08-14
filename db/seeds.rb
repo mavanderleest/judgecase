f036dc1d4ab7b5142e6626de72b49b13cd7f2132
Morgan <vdl@vMacBook.local>
2017-08-14 09:09:37 -0400

    Add conditional so Businesses only seed if the Business table is empty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # Import Parters from lib/seeds/businesses-seed
 require 'csv'
 
-CSV.foreach("lib/seeds/businesses-seed.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-  Business.create(row.to_hash)
+if Business.count == 0
+  CSV.foreach("lib/seeds/businesses-seed.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Business.create(row.to_hash)
+  end
 end
